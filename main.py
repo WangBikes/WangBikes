2e83e16e3df9d289453293c0f3cc2c365111015e
Xichao <>
2021-04-16 17:05:26 +0000

    buttons

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ class login_screen(Tk):
     
     self.lblUsername = Label(self, text='Username', font='arial 15 bold') 
     self.lblPassword = Label(self, text='Password', font='arial 15 bold') 
+
     self.entryUsername = Entry(self)
     self.entryPassword = Entry(self,show='*')
-    self.lblUsername.place(x=20,y=40) 
+
+    self.buttonLogin = Button(self,text='Login')
+    self.buttonSignup = Button(self,text='Sign up')
+
+    self.lblUsername.place(x=20,y=50) 
     self.lblPassword.place(x=20,y=100)
-    self.entryUsername.place(x=140,y=80)
-    self.entryPassword.place(x=140,y=105)
+
+    self.entryUsername.place(x=140,y=50)
+    self.entryPassword.place(x=140,y=100)
+
+    self.buttonLogin.place(x=100,y=150)
+    self.buttonSignup.place(x=200,y=150)
 
 
 if __name__ == '__main__':
