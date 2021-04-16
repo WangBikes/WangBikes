c2da753dd90999c60292c1fc13e04c94f08d1a82
Xichao <>
2021-04-16 14:09:52 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# TheBikeShop
+
+Team 1:
+Rory Solan-Jones
+Stefan Pelpola
+Joshua Baird
+Joshua Abadies
+Xichao Wang
+Requirements:
+Allow a user or admin to log in.
+Allow a user to select a bike to buy.
+Allow an Administrator to add Bikes and User.
+
+Jobs:
+Rory - Captain/Management
+Stefan - Lead GUI Designer
+Josh B - Assistant Programmer
+Josh A - Assistant GUI Designer
+Xichao - Lead Programmer 
+
+
+
+Layout:
+<enter username/email:>
+<enter password:>
+
+<Don’t have an account?>         <Sign In Here>      
+<Sign Up Here!>   
--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-with open database.json as database:
-  password
+from tkinter import * # import the tkinter module for gui
+
+class login_screen(Tk):
+  def __init__(self):
+    super().__init__()
+    self.geometry('400x400')
+    self.title('log in')
+    self.resizable(0,0)
+
+    
+    self.lblUsername = Label(self, text='Username', font='arial 15 bold') 
+    self.lblPassword = Label(self, text='Password', font='arial 15 bold') #create a label for passwords
+    self.lblUsername.place(x=20,y=40) #place the username and password labels
+    self.lblPassword.place(x=20,y=100)
+
+    self.entUsername = Label(self, text='', font='arial 15 bold') #create a label for username
+    self.entPassword = Label(self, text='', font='arial 15 bold') #create a label for passwords
+    self.entUsername.place(x=120,y=40) #place the username and password labels
+    self.entPassword.place(x=120,y=100)
+#login screen
+#login function
+#register
+if __name__ == '__main__': #when main.py is ran do the following
+  myWindow = login_screen()
+  myWindow.mainloop()
+  
